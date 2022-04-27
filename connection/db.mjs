9aa0a1ec92ae41e37306f89f6d073f3c9e2f31a5
YadBro <ncada708@gmail.com>
2022-04-27 09:51:42 +0700

    Task Day 8

---
+++ connection/db.mjs
@@ -0,0 +1,10 @@
+// import { Pool } from "pg";
+
+
+// // Setup connection pool
+// export const dbPool = new Pool({
+//     databse: 'b34s-chapter2',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'yadiprime009'
+// });
--- index.mjs
+++ index.mjs
@@ -8,7 +8,11 @@ import {
 import hbs from "hbs";
 import path from 'path';
 import bodyParser from "body-parser";
-import moment from 'moment';
+import {calculateDate} from 'calculating-date';
+
+// const calculated = require("calculating-date");
+
+// import db from "./connection/db.mjs";
 
 // const express = require('express');
 
@@ -45,10 +49,30 @@ hbs.registerHelper('isTrue', function (con, title) {
 });
 
 
+const blogs = [{
+    date: {
+    day: '26 Day',
+    month: '6 Month',
+    year: '1 Year' },
+    formInput: {
+        inputName: 'sadasdasd',
+        inputStartDate: '2022-03-31',
+        inputEndDate: '2023-10-18',
+        inputDescription: 'sadasdas',
+        nodeJsTechnology: 'on',
+        reactJsTechnology: 'on',
+        nextJsTechnology: 'on',
+        typeScriptTechnology: 'on',
+        inputFile: 'Muehehehe.jpg'
+    }
+}]
+
 
 app.get('/', function (req, res) {
+    let query = 
     res.render('index', {
-        title: "Home"
+        title: "Home",
+        blogs
     });
 });
 
@@ -65,24 +89,64 @@ app.get('/project', function (req, res) {
     });
 });
 
-// app.post('/')
-
 app.post('/project', function (req, res){
-    let startDate = moment(req.body.inputStartDate);
-    let endDate = moment(req.body.inputEndDate);
-    const day = endDate.diff(startDate, "day");
-    const month = endDate.diff(startDate, "month");
-    const year = endDate.diff(startDate, "year");
-
+    let startDate = req.body.inputStartDate;
+    let endDate = req.body.inputEndDate;
+    let calculated =  calculateDate(startDate, endDate)[0];
     const data = {
-        date        : {day, month, year},
+        date        : {
+            day: calculated.dayDiff,
+            month: calculated.monthDiff,
+            year: calculated.yearDiff
+        },
         formInput   : req.body
     }
 
-    console.log(data);
+
+    blogs.push(data);
+    res.redirect('/');
+});
+
+
+app.get('/detail-project/:id', function (req, res) {
+    const updatedId = req.params.id;
+    let getData = [blogs[updatedId]];
+    res.render('project-detail', {
+        title: "Project Detail",
+        getData
+    })
+});
+
+app.get('/update/:id', function (req, res){
+    const updatedId = req.params.id;
+    const getData = blogs[updatedId];
+    res.render('project-update', {getData,updatedId});
+});
+app.post('/update/:id', function (req, res){
+    const updatedId = req.params.id;
+
+    // UPDATING DATA
+    [blogs[updatedId]].map(m => {
+        m.formInput.inputName= req.body.inputName,
+        m.formInput.inputStartDate= req.body.inputStartDate,
+        m.formInput.inputEndDate= req.body.inputEndDate,
+        m.formInput.inputDescription= req.body.inputDescription,
+        m.formInput.nodeJsTechnology= req.body.nodeJsTechnology,
+        m.formInput.reactJsTechnology= req.body.reactJsTechnology,
+        m.formInput.nextJsTechnology= req.body.nextJsTechnology,
+        m.formInput.typeScriptTechnology= req.body.typeScriptTechnology,
+        m.formInput.inputFile= req.body.inputFile
+    });
+    res.redirect('/');
+});
+app.get('/delete/:id', function (req, res){
+    const deletedId = req.params.id;
+    blogs.splice(deletedId, 1);
+    res.redirect('/#myproject');
 });
 
 const port = 5000;
 app.listen(port, function () {
     console.log(`Server running on port : ${port}`);
 });
+
